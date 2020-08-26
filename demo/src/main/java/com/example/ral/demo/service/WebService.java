1b03fc8ea367f124cc5c3c6af746343e71c67335
renyunhui <1365305148@qq.com>
2020-08-26 15:11:31 +0800

    远程调试SpringBoot项目 jar包格式

---
+++ demo/src/main/java/com/example/ral/demo/service/WebService.java
@@ -0,0 +1,18 @@
+/**
+ * @projectName demo
+ * @package com.example.ral.demo.service
+ * @className com.example.ral.demo.service.WebService
+ * @copyright Copyright 2020 Thunisoft, Inc All rights reserved.
+ */
+package com.example.ral.demo.service;
+
+/**
+ * WebService
+ * @description WebService
+ * @author RenYunHui
+ * @date 2020/8/26 14:59
+ * @version 1.0
+ */
+public class WebService {
+
+}
